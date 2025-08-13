ee3912e30082dd100056fe7bb7a35ddf5215fdb0
KianK1 <epicgreenturtles@gmail.com>
2025-08-13 12:34:03 -0600

    Adds two numbers using a function

---
+++ 10-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add(a, b):
+    result = a + b
+    print("{} + {} = {}".format(a, b, result))
+a = int(input("First number: "))
+b = int(input("Second number: "))
+add(a, b)
